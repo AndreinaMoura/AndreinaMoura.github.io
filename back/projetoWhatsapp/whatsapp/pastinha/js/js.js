535c8b7311e875428220329d9522561b6c5fd810
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2022-09-08 21:37:59 -0300

    projeto

---
+++ back/projetoWhatsapp/whatsapp/pastinha/js/js.js
@@ -0,0 +1,314 @@
+var contatos = [
+  {
+    "img": "https://64.media.tumblr.com/4666c0c353577240d4c78c873b79a5b0/57f9c77312a6ba1f-94/s1280x1920/7525239899b3b6ab05dcf3f9d2d3bc82868945a6.jpg",
+    "nome": "Gojo",
+    "hora": "16:06",
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 1
+  },
+  {
+    "img": "https://i.pinimg.com/564x/1e/75/03/1e7503963b53dceb3917e90c1aba5389.jpg",
+    "nome": "Tornado",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 2
+  },
+  {
+    "img": "https://i.pinimg.com/736x/82/ff/f2/82fff2b55ff0d748206cbdf5e6ad526e.jpg",
+    "nome": "Rysand",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 3
+  },
+  {
+    "img": "https://i.pinimg.com/originals/11/6c/c8/116cc80b199e5f49f787adf3156a7fad.jpg",
+    "nome": "Mor",
+    "hora": "16:06",
+    "mensagem": "batata",
+    "visu": "./img/img4.png",
+    "id": 4
+  },
+  {
+    "img": "https://i.pinimg.com/originals/d3/b7/40/d3b740a5f89a9dc613291d6af436304b.jpg",
+    "nome": "Aelin",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 5
+  },
+  {
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95568d85-3ba8-45a1-8033-bc78515bb215/deo4n5u-3c5c6034-43da-4d98-8320-44408534a234.jpg/v1/fill/w_751,h_1063,q_70,strp/azriel_acotar_fanart_by_mdonaflor_deo4n5u-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTgxMSIsInBhdGgiOiJcL2ZcLzk1NTY4ZDg1LTNiYTgtNDVhMS04MDMzLWJjNzg1MTViYjIxNVwvZGVvNG41dS0zYzVjNjAzNC00M2RhLTRkOTgtODMyMC00NDQwODUzNGEyMzQuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.erqXsqdlUbxkccHW_JkAJjuzMAfQl6J4-qWJWLfhZJk",
+    "nome": "Azriel",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 6
+  },
+  {
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8a2d9d7d-044d-41c0-855a-f67ccc22f2d1/de43x4s-2b396bf7-ef80-4c12-a290-eeb0b7c73480.jpg/v1/fill/w_1024,h_1280,q_75,strp/inej_ghafa_by_saracia_de43x4s-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzhhMmQ5ZDdkLTA0NGQtNDFjMC04NTVhLWY2N2NjYzIyZjJkMVwvZGU0M3g0cy0yYjM5NmJmNy1lZjgwLTRjMTItYTI5MC1lZWIwYjdjNzM0ODAuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.6fRzTH5GH6b7vje_-Qs8_SZa1u8Cg0ZCpEdAqtzi5Rc",
+    "nome": "Inej",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 7
+  },
+  {
+    "img": "https://i.pinimg.com/originals/60/85/a4/6085a4c4a220506df8c8f4631592fb1a.jpg",
+    "nome": "Nico di angelo",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 8
+  },
+  {
+    "img": "https://64.media.tumblr.com/4666c0c353577240d4c78c873b79a5b0/57f9c77312a6ba1f-94/s1280x1920/7525239899b3b6ab05dcf3f9d2d3bc82868945a6.jpg",
+    "nome": "Gojo",
+    "hora": "16:06",
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 1
+  },
+  {
+    "img": "https://i.pinimg.com/564x/1e/75/03/1e7503963b53dceb3917e90c1aba5389.jpg",
+    "nome": "Tornado",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 2
+  },
+  {
+    "img": "https://i.pinimg.com/736x/82/ff/f2/82fff2b55ff0d748206cbdf5e6ad526e.jpg",
+    "nome": "Rysand",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 3
+  },
+  {
+    "img": "https://i.pinimg.com/originals/11/6c/c8/116cc80b199e5f49f787adf3156a7fad.jpg",
+    "nome": "Mor",
+    "hora": "16:06",
+    "mensagem": "batata",
+    "visu": "./img/img4.png",
+    "id": 4
+  },
+  {
+    "img": "https://i.pinimg.com/originals/d3/b7/40/d3b740a5f89a9dc613291d6af436304b.jpg",
+    "nome": "Aelin",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 5
+  },
+  {
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95568d85-3ba8-45a1-8033-bc78515bb215/deo4n5u-3c5c6034-43da-4d98-8320-44408534a234.jpg/v1/fill/w_751,h_1063,q_70,strp/azriel_acotar_fanart_by_mdonaflor_deo4n5u-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTgxMSIsInBhdGgiOiJcL2ZcLzk1NTY4ZDg1LTNiYTgtNDVhMS04MDMzLWJjNzg1MTViYjIxNVwvZGVvNG41dS0zYzVjNjAzNC00M2RhLTRkOTgtODMyMC00NDQwODUzNGEyMzQuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.erqXsqdlUbxkccHW_JkAJjuzMAfQl6J4-qWJWLfhZJk",
+    "nome": "Azriel",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 6
+  },
+  {
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8a2d9d7d-044d-41c0-855a-f67ccc22f2d1/de43x4s-2b396bf7-ef80-4c12-a290-eeb0b7c73480.jpg/v1/fill/w_1024,h_1280,q_75,strp/inej_ghafa_by_saracia_de43x4s-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzhhMmQ5ZDdkLTA0NGQtNDFjMC04NTVhLWY2N2NjYzIyZjJkMVwvZGU0M3g0cy0yYjM5NmJmNy1lZjgwLTRjMTItYTI5MC1lZWIwYjdjNzM0ODAuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.6fRzTH5GH6b7vje_-Qs8_SZa1u8Cg0ZCpEdAqtzi5Rc",
+    "nome": "Inej",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 7
+  },
+  {
+    "img": "https://64.media.tumblr.com/4666c0c353577240d4c78c873b79a5b0/57f9c77312a6ba1f-94/s1280x1920/7525239899b3b6ab05dcf3f9d2d3bc82868945a6.jpg",
+    "nome": "Gojo",
+    "hora": "16:06",
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 1
+  },
+  {
+    "img": "https://i.pinimg.com/564x/1e/75/03/1e7503963b53dceb3917e90c1aba5389.jpg",
+    "nome": "Tornado",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 2
+  },
+  {
+    "img": "https://i.pinimg.com/736x/82/ff/f2/82fff2b55ff0d748206cbdf5e6ad526e.jpg",
+    "nome": "Rysand",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 3
+  },
+  {
+    "img": "https://i.pinimg.com/originals/11/6c/c8/116cc80b199e5f49f787adf3156a7fad.jpg",
+    "nome": "Mor",
+    "hora": "16:06",
+    "mensagem": "batata",
+    "visu": "./img/img4.png",
+    "id": 4
+  },
+  {
+    "img": "https://i.pinimg.com/originals/d3/b7/40/d3b740a5f89a9dc613291d6af436304b.jpg",
+    "nome": "Aelin",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 5
+  },
+  {
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95568d85-3ba8-45a1-8033-bc78515bb215/deo4n5u-3c5c6034-43da-4d98-8320-44408534a234.jpg/v1/fill/w_751,h_1063,q_70,strp/azriel_acotar_fanart_by_mdonaflor_deo4n5u-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTgxMSIsInBhdGgiOiJcL2ZcLzk1NTY4ZDg1LTNiYTgtNDVhMS04MDMzLWJjNzg1MTViYjIxNVwvZGVvNG41dS0zYzVjNjAzNC00M2RhLTRkOTgtODMyMC00NDQwODUzNGEyMzQuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.erqXsqdlUbxkccHW_JkAJjuzMAfQl6J4-qWJWLfhZJk",
+    "nome": "Azriel",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 6
+  },
+  {
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8a2d9d7d-044d-41c0-855a-f67ccc22f2d1/de43x4s-2b396bf7-ef80-4c12-a290-eeb0b7c73480.jpg/v1/fill/w_1024,h_1280,q_75,strp/inej_ghafa_by_saracia_de43x4s-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzhhMmQ5ZDdkLTA0NGQtNDFjMC04NTVhLWY2N2NjYzIyZjJkMVwvZGU0M3g0cy0yYjM5NmJmNy1lZjgwLTRjMTItYTI5MC1lZWIwYjdjNzM0ODAuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.6fRzTH5GH6b7vje_-Qs8_SZa1u8Cg0ZCpEdAqtzi5Rc",
+    "nome": "Inej",
+    "hora": "16:06",
+    "mensagem": "Mensagem",
+    "visu": "./img/img4.png",
+    "id": 7
+  }
+]
+
+var conversas = [
+  {
+    "mensagem": "Eiiiiii",
+    "hora": "16:06",
+    "visu": "./img/img4.png",
+    "id": 1
+  },
+  {
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 2
+  },
+  {
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 3
+  },
+  {
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 4
+  },
+  {
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 5
+  },
+  {
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 6
+  },
+  {
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 7
+  },
+  {
+    "mensagem": "Eiiiiii",
+    "visu": "./img/img4.png",
+    "id": 8
+  }
+]
+
+var item = document.querySelector(".bloco");
+
+function ctt() {
+  contatos.forEach(cada => {
+    let novo = item.cloneNode(true)
+
+    novo.classList.remove("modelo")
+    let info = {
+      "img": cada.img,
+      "nome": cada.nome,
+      "id": cada.id
+    }
+
+    novo.querySelector(".imagem").src = cada.img;
+    novo.querySelector("#nome").innerHTML = cada.nome;
+    novo.querySelector(".hora").innerHTML = cada.hora;
+    novo.querySelector(".ms").innerHTML = cada.mensagem;
+    novo.querySelector(".visu").src = cada.visu;
+    cv()
+    window.localStorage.setItem('infoUser', JSON.stringify(info))
+    
+    document.querySelector("#principal").appendChild(novo);
+  })
+}
+
+// let zinho = document.querySelector(".inputzinho")
+// zinho.addEventListener("keyup", () => {
+//   contatos.forEach(cada => {
+//     if (cada.nome.toLowerCase().includes(zinho.value.toLowerCase())) {
+//       if (cada.mensagem.includes(zinho.value.toLowerCase())) {
+//         let id = cada.id;
+//         let vet = contatos[id];
+//         console.log(vet)
+//       }
+//     } else if (cada.nome == null) {
+//       console.log("não achei")
+//     } else {
+
+//     }
+//   });
+// });
+
+// document. addEventListener("keypress", function(e) {
+//   if(e. key === 'Enter') {
+//   var btn = document. querySelector("#submit")
+//   btn. click();
+//   }
+//   });
+
+var ct = document.querySelector(".man");
+
+function cv() {
+  var inf = JSON.parse(window.localStorage.getItem('infoUser'))
+
+  conversas.forEach(cada => {
+    let novo = item.cloneNode(true)
+
+    novo.classList.remove("modelo")
+    if (inf.id == cada.id) {
+      document.querySelector(".nome").addEventListener("click", cv => {
+        novo.querySelector("#foto").src = inf.img;
+        novo.querySelector(".ctNome").innerHTML = inf.nome;
+        novo.querySelector(".mtext").innerHTML = cv.hora;
+        novo.querySelector(".mhora").innerHTML = cv.mensagem;
+        novo.querySelector(".visu").src = cv.visu;
+
+        document.querySelector(".man").appendChild(novo);
+      });
+    }
+
+  })
+}
+function clickName() {
+
+}
+// // set interval
+// var tid = setInterval(myFunction(), 3000);
+// function myFunction() {
+//   // Get the snackbar DIV
+//   var x = document.getElementById("mensagem");
+
+//   // Add the "show" class to DIV
+//   x.className = "show";
+
+//   // After 3 seconds, remove the show class from DIV
+//   setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+//   tid = setTimeout(myFunction, 8000); // repeat myself
+// }
+// function abortTimer() { // to be called when you want to stop the timer
+//   clearInterval(tid);
+// }
---
+++ front/script.js
@@ -0,0 +1,138 @@
+var l
+var l2
+var l3
+var l4
+var l5
+var l6
+var l7
+var l8
+var l9
+var l10
+var l11
+
+document.querySelector('#mat').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "Linguagem de programação: Java";
+    document.querySelector('#mat').appendChild(l)
+})
+document.querySelector('#mat').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat').appendChild(l)
+})
+document.querySelector('#mat2').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "HTML";
+    l2 = document.createElement('p').innerHTML = "CSS";
+    document.querySelector('#mat2').append(l, l2)
+})
+document.querySelector('#mat2').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    l2 = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat2').append(l, l2)
+})
+document.querySelector('#mat3').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "Formatação";
+    document.querySelector('#mat3').appendChild(l)
+})
+document.querySelector('#mat3').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat3').appendChild(l)
+})
+document.querySelector('#mat4').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "Conceitos básicos";
+    document.querySelector('#mat4').appendChild(l)
+})
+document.querySelector('#mat4').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat4').appendChild(l)
+
+})
+document.querySelector('#mat5').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "Linguagens"
+    l2 = document.createElement('p').innerHTML = "JavaScript(Framework NodeJS)"
+    l3 = document.createElement('p').innerHTML = "PHP"
+    l4 = document.createElement('p').innerHTML = "Java(JSP) Servlet(FrameWOrk Spring)"
+    document.querySelector('#mat5').appendChild(l, l2, l3, l4)
+})
+document.querySelector('#mat5').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    l2 = document.createElement('p').innerHTML = "";
+    l3 = document.createElement('p').innerHTML = "";
+    l4 = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat5').appendChild(l, l2, l3, l4)
+})
+document.querySelector('#mat6').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "Linguagens"
+    l2 = document.createElement('p').innerHTML = "JavaScript"
+    l3 = document.createElement('p').innerHTML = "Marcação HTML, CSS"
+    l4 = document.createElement('p').innerHTML = "Framework(React), (JQuery), (BootStrap)"
+    document.querySelector('#mat6').appendChild(l, l2, l3, l4)
+})
+document.querySelector('#mat6').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    l2 = document.createElement('p').innerHTML = "";
+    l3 = document.createElement('p').innerHTML = "";
+    l4 = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat6').appendChild(l, l2, l3, l4)
+})
+document.querySelector('#mat7').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "Linguagens"
+    l2 = document.createElement('p').innerHTML = "SQL"
+    l3 = document.createElement('p').innerHTML = "DDL"
+    l4 = document.createElement('p').innerHTML = "DML"
+    l5 = document.createElement('p').innerHTML = "DCL"
+    l6 = document.createElement('p').innerHTML = "DQL"
+    l7 = document.createElement('p').innerHTML = "DTL"
+    l8 = document.createElement('p').innerHTML = "Importação e exportação de dados"
+    l9 = document.createElement('p').innerHTML = "CSV"
+    l10 = document.createElement('p').innerHTML = "XML"
+    l11 = document.createElement('p').innerHTML = "JSON"
+    document.querySelector('#mat7').append(l, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11)
+})
+document.querySelector('#mat7').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    l2 = document.createElement('p').innerHTML = "";
+    l3 = document.createElement('p').innerHTML = "";
+    l4 = document.createElement('p').innerHTML = "";
+    l5 = document.createElement('p').innerHTML = "";
+    l6 = document.createElement('p').innerHTML = "";
+    l7 = document.createElement('p').innerHTML = "";
+    l8 = document.createElement('p').innerHTML = "";
+    l9 = document.createElement('p').innerHTML = "";
+    l10 = document.createElement('p').innerHTML = "";
+    l11 = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat7').append(l, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11)
+})
+document.querySelector('#mat8').addEventListener('click', () => {
+    l = document.createElement('p').innerHTML = "Conceitos básicos";
+    l2 = document.createElement('p').innerHTML = "Linguagens"
+    l3 = document.createElement('p').innerHTML = "JavaScript"
+    l4 = document.createElement('p').innerHTML = "Framework(React Native, Expo)"
+    l5 = document.createElement('p').innerHTML = "Programação em Bloco MIT AppInventor"
+    document.querySelector('#mat8').append(l, l2, l3, l4, l5)
+})
+document.querySelector('#mat8').addEventListener('mouseout', () => {
+    l = document.createElement('p').innerHTML = "";
+    l2 = document.createElement('p').innerHTML = "";
+    l3 = document.createElement('p').innerHTML = "";
+    l4 = document.createElement('p').innerHTML = "";
+    l5 = document.createElement('p').innerHTML = "";
+    document.querySelector('#mat8').append(l, l2, l3, l4, l5)
+})
+var md = document.querySelector('model')
+function projeto1() {
+
+    md.style.display = 'flex'
+    let img = document.createElement('img')
+    img.src = `"../img/img.png"`
+    img.src = `"../img/img.png"`
+
+
+}
+function projeto2() {
+
+}
+function projeto3() {
+
+}
+function projeto4() {
+
+}
