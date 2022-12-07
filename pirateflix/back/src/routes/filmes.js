9cc87592de74a31677e5c697cac59156e5a0d628
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2022-12-07 14:30:31 -0300

    projeto

--- pirateflix/back/src/routes/filmes.js
+++ pirateflix/back/src/routes/filmes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const filme = require('../controllers/filmes');
 
-router.post("/pirateflix/cadastrar", filme.cadastrarfilme);
+router.post("/pirateflix", filme.cadastrarfilme);
 router.get("/pirateflix/:titulo", filme.buscarAlgo);
 router.get("/pirateflix", filme.buscarFilmes);
 router.delete("/pirateflix/:id", filme.excluirFilme);
--- pirateflix/front/página2/index2.js
+++ pirateflix/front/página2/index2.js
@@ -1,11 +1,9 @@
 var itemFilme = document.querySelector(".corpo")
-var titulo = document.querySelector("#titulo")
-var lancamento = document.querySelector("#lancamento")
-var categoria = document.querySelector("#categoria")
-var cartaz = document.querySelector("#cartaz")
-var cadastrar = document.querySelector("#cadastrar")
+var addFilme = document.querySelector("#add")
+let cadastrar = document.querySelector("#cadastrar")
 
 function carregar() {
+    // alert(cad)
     fetch("http://localhost:3000/pirateflix")
         .then(response => {
             return response.json();
@@ -22,22 +20,27 @@ function carregar() {
 
                 nome.innerHTML = cada.titulo;
                 imagem.src = cada.imagem;
-
+                
                 document.querySelector("main").appendChild(novoItem);
 
             })
         })
-
 }
 
 cadastrar.addEventListener('click', () => {
+    let titulo = document.querySelector("#titulo")
+    let lancamento = document.querySelector("#lancamento")
+    let categoria = document.querySelector("#categoria")
+    let cartaz = document.querySelector("#cartaz")
+
     let cad = {
         "titulo": titulo.value,
         "lancamento": lancamento.value,
         "cartaz": categoria.value,
         "imagem": cartaz.value
     }
-    fetch("http://localhost:3000/pirateflix", {
+    alert(JSON.stringify(cad))
+    fetch('http://localhost:3000/pirateflix', {
         "method": 'POST',
         "headers": {
             "Content-Type": "application/json"
@@ -50,23 +53,24 @@ cadastrar.addEventListener('click', () => {
         .then(resp => {
             if (resp.titulo !== undefined) {
                 alert("Filme cadastrado com sucesso!");
-                // window.location.reload();
+                window.location.reload();
             } else {
                 alert("Falha ao cadastrar filme!")
+                window.location.reload();
             }
         })
 })
 
 let modal = document.querySelector("dialog")
 
-function abrirModal() {
-    modal.style.visibility = 'visible'
-    modal.showModal()
-    location.reload();
-}
-
 function fecharModal() {
     modal.style.visibility = 'hidden'
     modal.close()
 }
 
+addFilme.addEventListener('click', () => {
+    addFilme.style.backgroundColor = 'yellow'
+    addFilme.style.color = 'black';
+    modal.style.visibility = 'visible'
+    modal.showModal()
+})
