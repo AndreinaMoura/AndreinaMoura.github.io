e15835a4e202a53f18f55292257f540a1099aa7b
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2022-12-07 11:14:14 -0300

    projeto

--- pirateflix/front/página2/index2.js
+++ pirateflix/front/página2/index2.js
@@ -1,4 +1,9 @@
 var itemFilme = document.querySelector(".corpo")
+var titulo = document.querySelector("#titulo")
+var lancamento = document.querySelector("#lancamento")
+var categoria = document.querySelector("#categoria")
+var cartaz = document.querySelector("#cartaz")
+var cadastrar = document.querySelector("#cadastrar")
 
 function carregar() {
     fetch("http://localhost:3000/pirateflix")
@@ -19,20 +24,48 @@ function carregar() {
                 imagem.src = cada.imagem;
 
                 document.querySelector("main").appendChild(novoItem);
+
             })
         })
 
 }
 
+cadastrar.addEventListener('click', () => {
+    let cad = {
+        "titulo": titulo.value,
+        "lancamento": lancamento.value,
+        "cartaz": categoria.value,
+        "imagem": cartaz.value
+    }
+    fetch("http://localhost:3000/pirateflix", {
+        "method": 'POST',
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": JSON.stringify(cad)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(resp => {
+            if (resp.titulo !== undefined) {
+                alert("Filme cadastrado com sucesso!");
+                // window.location.reload();
+            } else {
+                alert("Falha ao cadastrar filme!")
+            }
+        })
+})
+
 let modal = document.querySelector("dialog")
 
-function abrirModal(){
+function abrirModal() {
     modal.style.visibility = 'visible'
     modal.showModal()
-    location. reload();
+    location.reload();
 }
 
-function fecharModal(){
+function fecharModal() {
     modal.style.visibility = 'hidden'
     modal.close()
 }
